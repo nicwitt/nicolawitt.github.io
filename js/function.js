002883efe41e516e78d5561f781c62b38e0efb35
nicolawitt <64621763+nicolawitt@users.noreply.github.com>
2022-06-03 02:28:38 +1000

    commit

--- js/function.js
+++ js/function.js
@@ -1,35 +1,17 @@
 // ----------------NAV BAR------------------
 
-function toggleResponsive() {
-  var navbar = document.getElementById("dropdown-categories");
- navbar.classList.toggle("responsive");
-} 
-
-function openNav() {
-  document.getElementById("sidenav").style.width = "20%";
-}
-
-/* Closes Side Navigation */
-function closeNav() {
-  document.getElementById("sideCart").style.width = "0";
-  document.getElementById("mySidenav").style.width = "0";
-}
 
 // ----------------SIDE NAV------------------
 
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-  }
-  
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
-  }
+  document.getElementById("mySidenav").style.width = "40vw";
+  document.body.style.backgroundColor = "#rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.body.style.backgroundColor = "white";
+}
 
 // ----------------FILTER BUTTONS------------------
 
