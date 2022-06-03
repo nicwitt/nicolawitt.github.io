2fa2a981ec61e17c0709d32dcfdc0b1a6b71990f
nicola witt <nicola.witt1@gmail.com>
2022-06-03 21:12:59 +1000

    commit

--- js/function.js
+++ js/function.js
@@ -18,6 +18,15 @@ function closeNav() {
 
 // ----------------ADD TO CART------------------
 
+function openCart() {
+  document.getElementById("myCart").style.width = "50vw";
+  document.body.style.backgroundColor = "#rgba(0,0,0,0.4)";
+}
+
+function closeCart() {
+  document.getElementById("myCart").style.width = "0";
+  document.body.style.backgroundColor = "white";
+}
 
 
 // ----------------CHECKOUT VALIDATION------------------
